0600bd0a3da5973f1c3f3b8b8f9272792253f527
Sebastian Berghofer <sebastian_berghofer@gmx.de>
2020-04-15 23:50:37 +0200

    firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+admin.initializeApp();
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello guuuuysss firebase yeah");
+});
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+    admin
+    .firestore()
+    .collection('posts')
+    .get()
+    .then(data => {
+        let posts = [];
+        data.forEach(doc => {
+            postMessage.push(doc.data());
+        });
+        return res.json(posts);
+    })
+    .catch(err => console.error(err));
 });
